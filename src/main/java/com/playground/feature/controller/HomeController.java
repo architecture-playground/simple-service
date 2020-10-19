d4ee6c1e8b5f00d2a606fe1aa660a31c8c5b0b93
BulgakovAnton <bylgakov.anton.a@gmailcom>
2020-10-19 19:04:51 +0300

    add AntonController

--- src/main/java/com/playground/feature/controller/HomeController.java
+++ src/main/java/com/playground/feature/controller/HomeController.java
@@ -1,13 +1,30 @@
 package com.playground.feature.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HomeController {
 
     @GetMapping("/")
     public String welcomePage() {
+        log.info("welcome page");
         return "Hello, k8s World!";
     }
+
+    @GetMapping("/helloworld")
+    public String helloWorldPage() {
+        log.info("we did request on the hello world page");
+        return "Hello world page!";
+    }
+
+    @GetMapping("/logpage")
+    public String loggPage() {
+        log.info("Logging page start here");
+        return "logging page";
+    }
+
 }
+
